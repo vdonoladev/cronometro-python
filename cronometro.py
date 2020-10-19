a69c6c1d9235b12db6700350f533810ac3c6953a
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:29:28 -0300

    Updates

--- cronometro.py
+++ cronometro.py
@@ -11,22 +11,22 @@ ct_hour = ""
 while i == 1:
     for looping in range(i):
         time.sleep(1)
-        second+=1
-        if (second>=60):
-            second=0
-            minute+=1
-        if (minute >=60):
-            minute=0
-            hour+=1
-        if (second <=9):
+        second += 1
+        if (second >= 60):
+            second = 0
+            minute += 1
+        if (minute >= 60):
+            minute = 0
+            hour += 1
+        if (second <= 9):
             ct_sec = "0%s"
         else:
             ct_sec = "%s"
-        if (minute <=9):
+        if (minute <= 9):
             ct_min = "0%s"
         else:
             ct_min = "%s"
-        if (hour <=9):
+        if (hour <= 9):
             ct_hour = "0%s"
         else:
             ct_hour = "%s"
